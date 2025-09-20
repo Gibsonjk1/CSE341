bec36cf294ef9b65df64dd753614e720b1169240
JasonGibson <jasonkgibson3@gmail.com>
2025-09-20 17:02:04 -0600

    fixing routing

--- app.js
+++ app.js
@@ -2,11 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const routes = require('./routes');
-const lesson1controller = require('./controllers/lesson1');
 
-app.get('/', lesson1controller.homeroute); 
+app.get('/', routes.homeroute); 
 
-app.get('/Prosper', lesson1controller.Prosper); 
+app.get('/Prosper', routes.Prosper); 
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,6 @@ const routes = require('express').Router();
 const lesson1controller = require('../controllers/lesson1');
 
 routes.get('/', lesson1controller.homeroute);
+routes.get('/Prosper', lesson1controller.Prosper);
 
 module.exports = routes;
